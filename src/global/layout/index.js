e8e3e0beae6b1c9e689dd7debc7269d8e0a5a4fd
Royal2690 <royalfalcon152@gmail.com>
2023-12-27 23:26:25 +0500

    body added

--- src/global/layout/index.js
+++ src/global/layout/index.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './header'
+import Banner from './Body'
 import Footer from './footer'
 const Layout = () => {
     return (
         <>
             <Header />
-
+            <Banner/>
             <Footer />
         </>
     )
