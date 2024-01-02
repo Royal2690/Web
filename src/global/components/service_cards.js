39c9a51cbb4ba33f6c84138124e7775897768ff0
Royal2690 <royalfalcon152@gmail.com>
2024-01-02 21:40:42 +0500

    service card completd

--- src/global/components/service_cards.js
+++ src/global/components/service_cards.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Card from '../../assests/images/home/service_card_oe.jpg'
-const ServiceCards = () => {
+const ServiceCards = ({ service_img, title }) => {
     return (
         <div className="service_card">
-            <img src={Card} width='100%' alt="" />
+            <img src={service_img ? service_img :''} width='100%' alt="" />
             <div className="over_lay">
                 <div className="content">
-                    <h2>Duct Cleaning</h2>
+                    <h2>{title ? title : ''}</h2>
                     <button>Book Now</button>
                 </div>
             </div>
--- src/pages/homePage/feature/ourServices.js
+++ src/pages/homePage/feature/ourServices.js
@@ -5,10 +5,10 @@ const OurServices = () => {
         <div className='ourService page_width'>
             <h1 className='global_title'><span> Explore our </span> Services</h1>
             <div className="card_grid">
-                <ServiceCards />
-                <ServiceCards />
-                <ServiceCards />
-                <ServiceCards />
+                <ServiceCards service_img='' title={'Duct Cleaning'} />
+                <ServiceCards service_img='' title='' />
+                <ServiceCards service_img='' title='' />
+                <ServiceCards service_img='' title='' />
             </div>
         </div>
     )
